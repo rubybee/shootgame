f4cf9efb210a779b3a1afd6fe23644a6397d298d
ruby bee <ruby bee@DESKTOP-FNJUP8L>
2018-05-28 00:12:35 +0900

    First version

---
+++ shootgame/src/shootgame/Mainclass.java
@@ -0,0 +1,13 @@
+package shootgame;
+
+public class Mainclass {
+	
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 600;
+	
+
+	public static void main(String[] args) {
+		new ShootGame();
+	}
+
+}
---
+++ shootgame/src/shootgame/ShootGame.java
@@ -0,0 +1,16 @@
+package shootgame;
+
+import javax.swing.JFrame;
+
+public class ShootGame extends JFrame{
+	
+	public ShootGame() {
+		setTitle("Shoot Game");
+		setSize(Mainclass.SCREEN_WIDTH, Mainclass.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+}
