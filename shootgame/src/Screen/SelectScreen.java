34d2f9ac9448ff3a8b97f6f29d28a509090a7559
ruby bee <ruby bee@DESKTOP-FNJUP8L>
2018-06-03 01:02:06 +0900

    update

--- shootgame/src/Screen/SelectScreen.java
+++ shootgame/src/Screen/SelectScreen.java
@@ -1,7 +1,10 @@
 package Screen;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 
 import javax.swing.ImageIcon;
 
@@ -13,7 +16,8 @@ public class SelectScreen implements Screen {
 
 	Image background;
 	
-	int map;
+	Font helpfont = new Font("Arial", Font.PLAIN, 30);
+	Font titlefont= new Font("Arial", Font.BOLD, 70);
 	
 	Sound entersound;
 	
@@ -21,16 +25,22 @@ public class SelectScreen implements Screen {
 		background = new ImageIcon(Mainclass.class.getResource("../image/Selectbackground.jpg")).getImage();
 	}
 	
-	@Override
-	public void screenDraw(Graphics2D g) {
-		g.drawImage(background, 0, 0, null);
-		
-	}
-
+	
 	public void pressESC() {
 		entersound = new Sound("enter sound.mp3", false);
 		entersound.start();
 		ShootGame.screenstatus = 0;
+	}
+
+	@Override
+	public void screenDraw(Graphics2D g) {
+		g.drawImage(background, 0, 0, null);
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.setFont(helpfont);
+		g.setColor(Color.WHITE);
+		g.drawString("press esc to escape", 825, 120);
+		g.setFont(titlefont);
+		g.drawString("Select Map", 30, 120);
 	}
 
 }
