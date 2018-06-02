8ed1071f24d54c8a8d97c1aecf4354c108dca14e
rubybee <38034674+rubybee@users.noreply.github.com>
2018-06-02 23:38:25 +0900

    test
    test

--- shootgame/src/Screen/GameScreen.java
+++ shootgame/src/Screen/GameScreen.java
@@ -4,7 +4,9 @@ import java.awt.Graphics2D;
 
 public class GameScreen extends Thread implements Screen{
 
-	
+	void GameScreen {
+		System.out.println("test");
+	}
 	
 	@Override
 	public void screenDraw(Graphics2D g) {
